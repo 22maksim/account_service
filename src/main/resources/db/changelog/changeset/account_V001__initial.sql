a79294d05c2e18312d9f494f9b3ddbc4344a5bff
Maks Russkikh <15maksim@mail.ru>
2024-12-15 16:16:21 +0300

    add account  table and balance without migrations

--- src/main/resources/db/changelog/changeset/account_V001__initial.sql
+++ src/main/resources/db/changelog/changeset/account_V001__initial.sql
@@ -1,5 +1,22 @@
-CREATE TABLE accoutns (
-    id PRIMARY KEY,
-    name VARCHAR(32),
+CREATE TABLE account (
+    id UUID DEFAULT uuid_generate_v4(),
+    owner_id bigint,
+    type smallint NOT NULL,
+    currency smallint NOT NULL,
+    status smallint NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    update_at TIMESTAMP DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+    close_at TIMESTAMP,
+    version INT NOT NULL
+);
 
-)
+CREATE TABLE balance (
+    id UUID DEFAULT uuid_generate_v4(),
+    account int,
+    authorization_balance bigint default 0,
+    current_balance bigint default 0,
+    created_at timestamptz DEFAULT current_timestamp,
+    update_at TIMESTAMP DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+    version INT NOT NULL
+);
+
